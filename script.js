8d94aea382b788d319b378e76f1a7f448ad4b13d
Sivaranjani Jayaprakash <shiva.ranjani83@gmail.com>
2022-06-02 21:21:45 -0400

    added time-coded jquery

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var today = moment();
-var currentHour = today.format('h A');
-var timeSlot = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+
+
+var timeSlot = [9,10,11,12,13,14,15,16,17];
 
 // Adding Today's date to header section using moment js
 $("#currentDay").text(today.format("dddd, MMMM Do"));
@@ -15,14 +16,30 @@ function displayEventsOnLoad() {
 displayEventsOnLoad();
 
 
-
 function saveEvents() {
     var targetElId = event.target.id.split("_")[1];
-
-
     localStorage.setItem(targetElId, $("#event_" + targetElId).val());
+}
 
 
+
+setInterval(addColor, 1000);
+
+function addColor(){
+for(i=0; i<timeSlot.length; i++){
+    var currentHour = moment().hour();
     
+   if( timeSlot[i] === currentHour){
+       $("#event_"+timeSlot[i]).addClass("present");
+   }
 
+   if( timeSlot[i] < currentHour){
+    $("#event_"+timeSlot[i]).addClass("past");
+}
+
+if( timeSlot[i] > currentHour){
+    $("#event_"+timeSlot[i]).addClass("future");
+}
+
+}
 }
